ce3fe69c499ba3f0a6d9f70b2d948b9a022d5a02
anas <anas@gmail.com>
2024-09-07 01:09:49 +0300

    تعديلات على urls.py وإضافة مسار للصفحة الرئيسية

--- labs/‏‏lab1/djangovenv/Scripts/activate.bat
+++ labs/‏‏lab1/djangovenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Computer\Desktop\Abdulrahman Al-Hubaishi\labs\‏‏lab1\djangovenv
+set VIRTUAL_ENV=C:\Users\Computer\Desktop\Abdulrahman-Al-Hubaishi\labs\‏‏lab1\djangovenv
 
 if not defined PROMPT set PROMPT=$P$G
 
